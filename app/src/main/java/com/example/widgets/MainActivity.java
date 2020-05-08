00d3a6e239ff94de7d9d9ba7b350b087f1386399
Oscar Drabik <a18oscdr@student.his.se>
2020-05-08 19:55:09 +0200

    added some widgets

--- app/src/main/java/com/example/widgets/MainActivity.java
+++ app/src/main/java/com/example/widgets/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.widgets;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button knapp = findViewById(R.id.bigbtn);
+
+        knapp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("==>","You clicked the button. Nice");
+            }
+        });
     }
-}
+
+    };
+
